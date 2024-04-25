b9465c65e85d249a589c2d5e062e5acd4bef218b
shuhei tanaka <shuhei46491@gmail.com>
2024-04-25 22:25:45 +0900

    decode data

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -4,7 +4,9 @@ export default createSchema((p) => ({
   RegisteredEvent: p.createTable({
     id: p.string(),
     recipientId: p.hex(),
-    data: p.hex(),
+    recipientAddress: p.hex(),
+    recipientCount: p.bigint(),
+    metadata: p.string(),
     sender: p.hex(),
     timestamp: p.bigint(),
   }),
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import { ponder } from "@/generated";
+import { ethers } from "ethers";
 
 ponder.on("AlloStrategy:Registered", async ({ event, context }) => {
   const { RegisteredEvent } = context.db;
-
+  const abiCoder = new ethers.AbiCoder();
+  const item = event.args.data;
+  const extendedData = abiCoder.decode(["bytes", "uint256"], item);
+  const decodedData = abiCoder.decode(
+    ["address", "address", "tuple(uint256, string)"],
+    extendedData[0]
+  );
   await RegisteredEvent.create({
     id: event.log.id,
     data: {
       recipientId: event.args.recipientId,
-      data: event.args.data,
+      recipientAddress: decodedData[1],
+      recipientCount: extendedData[1],
+      metadata: decodedData[2][1],
       sender: event.args.sender,
       timestamp: event.block.timestamp,
     },
